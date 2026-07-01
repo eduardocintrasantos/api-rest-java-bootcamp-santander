6cbbb85ac579e1e588e892e1f627ff03bb35448d
Eduardo Cintra dos Santos <eduardocintradossantos@gmail.com>
2026-07-01 14:42:59 -0300

    Update CreateTaskRequestDTO.java

--- src/main/java/dio/taskmanager/infrastructure/http/request/CreateTaskRequestDTO.java
+++ src/main/java/dio/taskmanager/infrastructure/http/request/CreateTaskRequestDTO.java
@@ -1,6 +1,11 @@
 package dio.taskmanager.infrastructure.http.request;
 
+import dio.taskmanager.application.input.CreateTaskInputDTO;
+
 import java.util.Optional;
 
 public record CreateTaskRequestDTO (String title, Optional<String> description){
+    public CreateTaskInputDTO toInputDTO() {
+        return new CreateTaskInputDTO(title, description);
+    }
 }
